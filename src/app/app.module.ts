3c03096a4a99617f973af535ecfe48f8a8ea6123
Manuel Sernaque <majesr1195@gmail.com>
2020-06-27 11:02:01 -0500

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { InicioComponent } from './pages/inicio/inicio.component';
+import { CategoriasComponent } from './pages/categorias/categorias.component';
+import { ServiciosComponent } from './pages/servicios/servicios.component';
+import { PedidosComponent } from './pages/pedidos/pedidos.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    InicioComponent,
+    CategoriasComponent,
+    ServiciosComponent,
+    PedidosComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
